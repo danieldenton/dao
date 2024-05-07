e187149112d4b136891478027afdbb53e6c30f50
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-05-07 15:58:52 -0700

    first 2 DAO tests

--- test/DAO.js
+++ test/DAO.js
@@ -7,47 +7,30 @@ const tokens = (n) => {
 
 const ether = tokens
 
-describe('Token', () => {
-  let token, accounts, deployer, receiver, exchange
+describe('DAO', () => {
+  let token, dao
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory('Token')
     token = await Token.deploy('Dapp University', 'DAPP', '1000000')
 
     const DAO = await ethers.getContractFactory('DAO')
-    let dao = await DAO.deploy(token.address, '500000000000000000000001')
+    dao = await DAO.deploy(token.address, '500000000000000000000001')
 
-    accounts = await ethers.getSigners()
-    deployer = accounts[0]
-    receiver = accounts[1]
-    exchange = accounts[2]
+   
   })
 
   describe('Deployment', () => {
-    const name = 'Dapp University'
-    const symbol = 'DAPP'
-    const decimals = '18'
-    const totalSupply = tokens('1000000')
 
-    it('has correct name', async () => {
-      expect(await token.name()).to.equal(name)
+    it('returns token addresss', async () => {
+      expect(await dao.token()).to.equal(token.address)
     })
 
-    it('has correct symbol', async () => {
-      expect(await token.symbol()).to.equal(symbol)
-    })
+    it('returns quorum', async () => {
+        expect(await dao.quorum()).to.equal('500000000000000000000001')
+      })
 
-    it('has correct decimals', async () => {
-      expect(await token.decimals()).to.equal(decimals)
-    })
-
-    it('has correct total supply', async () => {
-      expect(await token.totalSupply()).to.equal(totalSupply)
-    })
-
-    it('assigns total supply to deployer', async () => {
-      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
-    })
+   
 
   })
 
