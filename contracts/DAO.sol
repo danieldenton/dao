4d1b0aa758062c396888fddc8b6775af1a7df9f3
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-05-08 09:21:46 -0700

    Propsal created

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -9,11 +9,34 @@ contract DAO {
     Token public token;
     uint256 public quorum;
 
+     struct Proposal {
+        uint256 id;
+        string name;
+        uint256 amount;
+        address payable recipient;
+        uint256 votes;
+        bool finalized;
+    }
+
+    uint256 proposalCount;
+
     constructor(Token _token, uint256 _quorum) {
         owner = msg.sender;
         token = _token;
         quorum = _quorum;
     }
 
+    // external can only be called outside the contract. public could be called in or out.
     receive() external payable {}
+
+   
+
+    function createProposal(
+        string memory _name,
+        uint256 _amount,
+        address payable _recipient
+    ) external {
+        proposalCount++;
+        Proposal(proposalCount, _name, _amount, _recipient, 0, false);
+    }
 }
